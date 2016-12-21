a596397111929081132fe81b24d356ac9bb55d6b
Bruno Prado <bruno_cesar_l@hotmail.com>
2016-12-20 22:29:29 -0200

    ADD + SUB + SHR + NEG

--- js/simulador.js
+++ js/simulador.js
@@ -6,14 +6,14 @@ var SIMULADOR = function() {
         "N" : false,
         "Z" : false,
         "C" : false
-    }
+    };
     this.REGISTRADORES = {
         "PC": 0,
-        "a" : null,
-        "b" : null,
-        "c" : null,
-        "d" : null,
-        "x" : null
+        "a" : 0,
+        "b" : 0,
+        "c" : 0,
+        "d" : 0,
+        "x" : 0
     };
         
     this.proximaInstrucao = function() {
@@ -34,7 +34,7 @@ var SIMULADOR = function() {
                 } else {
                     valor = instrucoes[partes[2]];
                 }
-                this.REGISTRADORES[partes[1]] = valor;
+                this.REGISTRADORES[partes[1]] = parseInt(valor);
                 imprime("Registrador " + partes[1].toUpperCase() + " carregou o valor " + valor);
                 break;
                 
@@ -44,10 +44,46 @@ var SIMULADOR = function() {
                 imprime("Valor do registrador " + partes[1].toUpperCase() + " (" + this.REGISTRADORES[partes[1]] + ") enviado para o endereço " + partes[2]);
                 break;
                 
+            //ADICIONA    
+            case "add" :
+                var valor = 0;
+                if(partes[2].indexOf("#") == 0){
+                    valor = partes[2].substr(1,partes[2].length-1);
+                } else {
+                    valor = instrucoes[partes[2]];
+                }
+                this.REGISTRADORES[partes[1]] += parseInt(valor);
+                imprime("Valor do registrador " + partes[1].toUpperCase() + " += " + partes[2] + " (" + this.REGISTRADORES[partes[1]] + ")");
+                break;
+                
+            //SUBTRAI    
+            case "sub" :
+                var valor = 0;
+                if(partes[2].indexOf("#") == 0){
+                    valor = partes[2].substr(1,partes[2].length-1);
+                } else {
+                    valor = instrucoes[partes[2]];
+                }
+                this.REGISTRADORES[partes[1]] -= parseInt(valor);
+                imprime("Valor do registrador " + partes[1].toUpperCase() + " -= " + partes[2] + " (" + this.REGISTRADORES[partes[1]] + ")");
+                break;
+                
+            //NEGATIVO    
+            case "neg" :
+                this.REGISTRADORES[partes[1]] = 0 - this.REGISTRADORES[partes[1]];
+                imprime("Sinal do registrador " + partes[1].toUpperCase() + " invertido (" + this.REGISTRADORES[partes[1]] + ")");
+                break;    
+                
+            //SHIFT RIGHT    
+            case "shr" :
+                this.REGISTRADORES[partes[1]] >>= 1;
+                imprime("Valor do registrador " + partes[1].toUpperCase() + " deslocado para direita (" + this.REGISTRADORES[partes[1]] + ")");
+                break;
+                
             //ENCERRA    
             case "hlt" :
                 this.LIGADO = false;
-                imprime("HLT encontrado. Processamento encerrado");
+                imprime("<span style='color: #ce4949;'>HLT encontrado. Processamento encerrado</span>");
                 break;
         }
         this.REGISTRADORES.PC += 1;
